64207d4b60f3ac76e882e8bc1eaff564b4ff9d61
Omkar Pawar <opawar88@gmail.com>
2021-12-02 16:40:58 +0530

    try catch block

--- DecisionMaking/Program.cs
+++ DecisionMaking/Program.cs
@@ -7,29 +7,47 @@ namespace DecisionMaking
         static void Main(string[] args)
         {
             Console.Write("Enter Total Student :  ");
-            int total = Int32.Parse(Console.ReadLine());           
-            for (int i = 0; i < total; i++)
+            try
             {
-                Console.Write("Enter Student Name  :  ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Student Name : {0}", name);
-                Random random = new Random();
-                random.Next(1, 100);
-                int c = random.Next(1, 100);
-                int java = random.Next(1, 100);
-                int php = random.Next(1, 100);               
-                Console.WriteLine("Subject      : C#        Java         PHP ");
-                Console.WriteLine("Mark         : {0}        {1}          {2} ", php, java, c);
-                int avarage = (c + java + php) / 3;
-                Console.WriteLine("Avarage      : {0} ", avarage);
-                string result = "Fail";
-                if (avarage <= 35)
+                int total = Int32.Parse(Console.ReadLine());
+                int pass = 0;
+                int fail = 0;
+                for (int i = 0; i < total; i++)
                 {
-                    result = "Pass";
+                    Console.Write("Enter Student Name  :  ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Student Name : {0}", name);
+                    Random random = new Random();
+                    random.Next(1, 100);
+                    int c = random.Next(1, 100);
+                    int java = random.Next(1, 100);
+                    int php = random.Next(1, 100);
+                    Console.WriteLine("Subject      : C#        Java         PHP ");
+                    Console.WriteLine("Mark         : {0}        {1}          {2} ", php, java, c);
+                    int avarage = (c + java + php) / 3;
+                    Console.WriteLine("Avarage      : {0} ", avarage);
+                    string result = "Fail";
+                    if (avarage <= 35)
+                    {
+                        result = "Pass";
+                        pass = pass + 1;
+                    }
+                    if (result == "Fail")
+                    {
+                        fail = fail + 1;
+                    }
+
+                    Console.WriteLine("-----------------------------------------------------");
+                    Console.WriteLine("Result       : {0}", result);
+                    Console.WriteLine("-----------------------------------------------------");
                 }
                 Console.WriteLine("-----------------------------------------------------");
-                Console.WriteLine("Result       : {0}", result);
+                Console.WriteLine("Total Student : {0}   Total Pass : {1}  Total Fail : {2}", total, pass, fail);
                 Console.WriteLine("-----------------------------------------------------");
+            }
+            catch(FormatException)
+            {
+                Console.Write("Enter number only");
             }
             Console.Read();
         }
